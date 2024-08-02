07fb090acb9d9a7b842463996c543157e10c745a
Osama Eladawy <classcar200@gmail.com>
2024-08-02 10:34:25 +0300

    "create icon"

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -100,7 +100,7 @@ class _HomeState extends State<Home> {
             ),
           ),
           const SizedBox(
-            height: 20,
+            height: 20
           ),
           Text(
             result,
